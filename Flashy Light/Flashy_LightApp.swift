9af6662ee8df3b7e024b62e618f5f3bbc5b2d08b
Ash <4707734+Eiad@users.noreply.github.com>
2024-09-27 16:45:28 +0400

    Initial Commit

---
+++ Flashy Light/Flashy_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flashy_LightApp.swift
+//  Flashy Light
+//
+//  Created by Ash on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flashy_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
